9db31967799f729b951ae6f2d24b6b4d35c229dc
caseyfleck <casey.fleck@clearcaptions.com>
2022-02-04 14:56:45 -0800

    first commit

---
+++ src/main/java/com/caseyjofleck/simplegrocerylist/SimpleGroceryListApplication.java
@@ -0,0 +1,13 @@
+package com.caseyjofleck.simplegrocerylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGroceryListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleGroceryListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caseyjofleck/simplegrocerylist/SimpleGroceryListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caseyjofleck.simplegrocerylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGroceryListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
